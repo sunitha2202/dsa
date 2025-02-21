3ee031f20e625864ff61be5741ac98d83f68948c
sunitha <k28tamil@gmail.com>
2025-02-21 20:32:56 +0530

    problem 3 & 4

---
+++ problem3.py
@@ -0,0 +1,11 @@
+mark= int(input())
+if(mark>=90):
+    print("garde A")
+elif(mark>=80):
+    print("grade B")
+elif(mark>=60):
+    print("grade C")
+elif(mark>=35):
+    print("grade D")
+else:
+    print("fail")
---
+++ problem4.py
@@ -0,0 +1,11 @@
+size= int(input())
+if(size==29):
+    print("small")
+elif(size==30):
+    print("medium")
+elif(size==38):
+    print("large")
+elif(size==42):
+    print("XLarge")
+else:
+    print("size is invalid")
